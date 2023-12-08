a1adf32314793ebcdc88cb5f309420ffc624dde1
Dee Caulcrick <deecaulcrick@gmail.com>
2023-12-08 19:28:11 +0100

    said hi to Dabi and Teni

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,35 +1,41 @@
-import Link from 'next/link'
-import projectsData from '../../index/projects'
+import Link from "next/link";
+import projectsData from "../../index/projects";
 const projects = () => {
-    return (
-        <div className="projects">
-            <div className='hero-container'>
+  return (
+    <div className="projects">
+      <div className="hero-container">
+        <h1 className="page-title">
+          Projects<sup className="superscript">{projectsData.length}</sup>
+        </h1>
+        <p>
+          Here is a list of projects that made it to the internet: Hii Dabiolu
+          and Teni
+        </p>
+      </div>
+      <div className="project-list">
+        {projectsData.map((project) => (
+          <div className="project-card" key={project.id}>
+            <h2>
+              <Link
+                href={project.link}
+                target="_blank"
+              >{`${project.title} ↗`}</Link>
+            </h2>
 
-                <h1 className='page-title'>Projects<sup className='superscript'>{projectsData.length}</sup></h1>
-                <p>Here is a list of projects that made it to the internet:</p>
+            <p className="project-excerpt">{project.desc}</p>
 
+            <div className="category-box">
+              {project.tools.map((tool, id) => (
+                <div key={id} className="category">
+                  {tool}
+                </div>
+              ))}
             </div>
-            <div className="project-list">
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-                {projectsData.map((project) => (
-                    <div className='project-card' key={project.id}>
-                        <h2><Link href={project.link} target='_blank'>{`${project.title} ↗`}</Link></h2>
-
-                        <p className='project-excerpt'>{project.desc}</p>
-
-                        <div className="category-box">
-                            {project.tools.map((tool, id) => (
-                                <div key={id} className="category">{tool}</div>
-
-
-                            ))}
-                        </div>
-                    </div>
-                ))}
-
-            </div>
-        </div>
-    )
-}
-
-export default projects
+export default projects;
