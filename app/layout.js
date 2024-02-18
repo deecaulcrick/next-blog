f950506c573d63b9493450224e58a4aaf0dd6cde
Dee Caulcrick <deecaulcrick@gmail.com>
2024-02-18 18:16:31 +0100

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,23 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
-import './globals.css'
+import { Analytics } from "@vercel/analytics/react";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./globals.css";
 
 export const metadata = {
   title: "Dee Caulcrick's Website",
-  description: 'A blog on computer science and web development',
-}
+  description: "A blog on computer science and web development",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
       <body>
         <Header />
-        <div className='app-body'>
-          {children}
-        </div>
+        <div className="app-body">{children}</div>
         <Footer />
-
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
